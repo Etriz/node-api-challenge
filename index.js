0529dfcf70ed0b6bff729ee65a8fce01c1b83cfa
ryanp <ryanpanimation@gmail.com>
2020-07-17 20:28:06 -0500

    setup index

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require("./server.js");
+
+const PORT = process.env.PORT || 5000;
+server.listen(5000, () => {
+  console.log("Server listening on localhost:", PORT);
+});
